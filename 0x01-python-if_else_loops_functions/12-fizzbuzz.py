d5ece36f58e3d49c0c955efd4dbff8e7fc5277c3
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-08 13:25:54 -0500

    Fix type errors and pep8 style

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,18 +4,19 @@
 def fizzbuzz():
     """Prints the numbers from 1 to 100 separated by a space
 
-    For multiples of three print Fizz instead of the number and for multiples of five print Buzz.
+    For multiples of three print Fizz instead of the number and for multiples\
+    of five print Buzz.
     For numbers which are multiples of both three and five print FizzBuzz.
     """
 
-    for num in range(1,101):
-        if num % 15:
+    for num in range(1, 101):
+        if num % 15 == 0:
             print("FizzBuzz ", end="")
-        elif num % 3:
+        elif num % 3 == 0:
             print("Fizz ", end="")
-        elif num % 5:
-            print("Buzz", end="")
+        elif num % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print("{d} ".format(num), end="")
+            print("{:d} ".format(num), end="")
 
     return (None)
