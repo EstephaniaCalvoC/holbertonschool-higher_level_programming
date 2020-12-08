58e3e9df5ab43249ac12fbe7e02feba1027aed74
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-08 12:27:39 -0500

    Change print mode for requirement

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (i == 101 or i == 113):
+
+for char in range(97, 123):
+    if (char == 101 or char == 113):
         continue
-    print(chr(i), end="")
+    print("{}".format(chr(char)), end="")
