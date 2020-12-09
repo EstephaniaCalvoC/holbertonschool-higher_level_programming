e023d9e1fd45f75fd15a60313f6662e714996233
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-08 22:40:39 -0500

    Fix case: n negative in 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,4 +4,6 @@
 def remove_char_at(str, n):
     """Create a copy of the string, removing the character at the position n"""
 
+    if n < 0:
+        return(str)
     return (str[:n] + str[n + 1:])
