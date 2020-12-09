7aaa64edb5fd704ebc06a88ae5223ee42dfff946
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-08 22:29:26 -0500

    Add 102-magic_calculation.py function

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+def magic_calculation(a, b, c):
+    """Do exactly the same Python bytecod provide by Holberton"""
+
+    if (a < b):
+        return(c)
+    elif (c > b):
+        return (a + b)
+    else:
+        return (a * b - c)
