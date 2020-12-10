6fabe49245c58ce1af6c093199297563b31d4a26
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-10 18:38:20 +0000

    Change elif for if

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     if (argc != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif (argv[2] not in list(calc.keys())):
+
+        if (argv[2] not in list(calc.keys())):
         print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
 
