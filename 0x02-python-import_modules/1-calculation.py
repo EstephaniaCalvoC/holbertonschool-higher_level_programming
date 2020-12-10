763bd6df58bf38db45ce929d8cc16ac69ea51f64
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-10 17:42:34 +0000

    Change algorithm

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -10,9 +10,8 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    operations = [['+', add], ['-', sub], ['*', mul], ['/', div]]
 
-    for i in operations:
-        operator = i[0]
-        fun = i[1]
-        print("{} {} {} = {}".format(a, operator, b, fun(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
