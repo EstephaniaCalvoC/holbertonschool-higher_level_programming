b2f1b43e3ed578d79557bbf080e50e6df8d9d510
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-10 20:21:52 +0000

    Add 102-magic_calculation.py script

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+def magic_calculation(a, b):
+    """Do exactly the same Python function bytecode provide by Holberton"""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return(sub(a, b))
