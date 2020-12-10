b42600ee976ba46f5b14bf6c24ba172ca428e942
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-10 19:49:05 +0000

    Test

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     cal = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    if sys.argv[2] not in list(cal.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
