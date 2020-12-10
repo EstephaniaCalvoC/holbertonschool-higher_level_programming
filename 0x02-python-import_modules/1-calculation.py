0025717ff9af85f1043339a36b671bc0e54b5cc8
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-10 18:28:35 +0000

    Remove comments

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """
-    Import functions from a file and does some Maths,
-    and prints the result
-    """
     from calculator_1 import add, sub, mul, div
 
     a = 10
