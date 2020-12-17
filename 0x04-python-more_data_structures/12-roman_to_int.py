ea24fb6dbd6dd02c94e18377a894d19810123ffd
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-17 15:57:34 -0500

    Fix verify corret input and pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,13 +4,13 @@ def roman_to_int(roman_string):
 
     dic_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    if not roman_string or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string == None:
         return (0)
 
     num = 0
     current = dic_num.get(roman_string[0], 0)
 
-    for i in roman_string[1 :]:
+    for i in roman_string[1:]:
         next_n = dic_num.get(i, 0)
 
         if (current == 0 or next_n == 0):
