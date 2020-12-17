11da27e11d6fdf6e630bdea189344fe42d1f31f1
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-17 16:24:10 -0500

    Fix error: Verify empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,5 +2,9 @@
 def weight_average(my_list=[]):
     """Return the weighted average of all integers tuple (<score>, <weight>)"""
 
-    av = sum([i[0] * i[1] for i in my_list]) / sum([i[1] for i in my_list])
+    av = 0
+
+    if my_list:
+        av = sum([i[0] * i[1] for i in my_list]) / sum([i[1] for i in my_list])
+
     return av
