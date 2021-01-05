cda2437316de9ea863d0e9f5dcf668e7791ff829
EstephaniaCalvoC <2177@holbertonschool.com>
2021-01-05 17:14:57 -0500

    Add 103-magic_class.py class

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+'''
+Define a class MagicClass.
+Classes:
+MagicClass
+'''
+import math
+
+
+class MagicClass():
+    '''
+    A class to represent a circle.
+
+    Atributes
+    ---------
+        __radius: int or float
+            radius of the circle.
+    Methods
+    -------
+        area():
+            Calculate the area of a circle.
+        circunference():
+            Calculate the perimeter of a circle.
+    '''
+
+    def __init__(self, radius=0):
+        '''
+        Construct all the necessary attributes for the circle object.
+
+        Parameters
+        ----------
+            radius: int
+                radius of the square, number positive for default is 0.
+        '''
+
+        if type(radius) is not float and type(radius) is not int:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        '''Calculate circle's area'''
+
+        return math.pi * 2 ** self.__raduis
+
+    def circunference(self):
+        '''Calculate circle's perimeter'''
+
+        return math.pi * 2 * self.__radius
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -74,11 +74,11 @@ What I learned from this project:
 
 ### [9. Compare 2 squares](./102-square.py)
 * Write a class Square that defines a square by: (based on 4-square.py)
-
+-->
 
 ### [10. ByteCode -> Python #5](./103-magic_class.py)
-* Write the Python class MagicClass that does exactly the same as the following Python bytecode:
--->
+* A Python lass MagicClass that does exactly the same Python bytecode provide for Holberton.
+
 
 ---
 
