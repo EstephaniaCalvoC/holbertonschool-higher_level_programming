3ec98c7e49b962e84947150182d823eb11ad5f0c
EstephaniaCalvoC <2177@holbertonschool.com>
2021-01-20 19:23:51 -0500

    Fix type

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,9 +4,9 @@
 
 def class_to_json(obj):
     """
-    Return the dictionary description with simple data structure 
-    (list, dictionary, string, integer and boolean) for JSON 
+    Return the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON
     serialization of an object.
     """
 
-    return obj.__dic__
+    return obj.__dict__
