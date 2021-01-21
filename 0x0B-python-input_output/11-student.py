dce6585944b73ad6b9b026e351d5524fa5339150
EstephaniaCalvoC <2177@holbertonschool.com>
2021-01-20 22:34:08 -0500

    Fix pep8 style

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -14,7 +14,7 @@ class Student:
     def to_json(self, attrs=None):
         """Retrieve a dictionary representation of a Student instance"""
 
-        if type(attrs) == list and all (type(i) == str for i in attrs):
+        if type(attrs) == list and all(type(i) == str for i in attrs):
             k_v = [[k, getattr(self, k)] for k in attrs if hasattr(self, k)]
             return dict(k_v)
         return self.__dict__
