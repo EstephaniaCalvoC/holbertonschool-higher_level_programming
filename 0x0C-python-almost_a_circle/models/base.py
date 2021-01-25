26c38f87facd725acbc8473a7cd20d78d310f44f
EstephaniaCalvoC <2177@holbertonschool.com>
2021-01-24 21:30:52 -0500

    Add to_dictionary function to Square class task->14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,4 +36,18 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
+
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file"""
+
+        f_name = cls.__name__ + ".json"
+
+        with open(f_name, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                l_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(Base.to_json_string(l_dicts))
--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -6,6 +6,7 @@ import os
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase_init(unittest.TestCase):
@@ -32,13 +33,13 @@ class TestBase_init(unittest.TestCase):
 class TestBase_to_json_string(unittest.TestCase):
     """Test cases for to_json_string method in Base class"""
 
-    def test_list_None(self):
+    def test_list_dict_None(self):
         self.assertEqual("[]", Base.to_json_string(None))
 
-    def test_list_empty(self):
+    def test_list_dict_empty(self):
         self.assertEqual("[]", Base.to_json_string([]))
 
-    def test_correct_list(self):
+    def test_correct_list_dict(self):
         r1 = Rectangle(10, 7, 2, 8)
         dic1 = r1.to_dictionary()
         r2 = Rectangle(20, 14, 4, 16)
@@ -52,4 +53,68 @@ class TestBase_to_json_string(unittest.TestCase):
             {"x": 4, "width": 20, "id": r2.id, "height": 14, "y": 16}
             ]
         real = eval(json_dictionary)
-        self.assertEqual(exp, real) 
+        self.assertEqual(exp, real)
+
+
+class TestBase_save_to_file(unittest.TestCase):
+    """Test cases for save_to_file method in Base class"""
+
+    @classmethod
+    def clean_files(self):
+        """Clean any 'class.json' files"""
+
+        for _class in (Base, Rectangle, Square):
+            try:
+                os.remove(_class.__name__ + ".json")
+            except IOError:
+                pass
+
+    def test_list_obj_None(self):
+        Square.save_to_file(None)
+        with open("Square.json", "r") as f:
+            self.assertEqual("[]", f.read())
+
+    def test_list_obj_empty(self):
+        Square.save_to_file([])
+        with open("Square.json", "r") as f:
+            self.assertEqual("[]", f.read())
+
+    def test_correct_list_obj(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+
+        Rectangle.save_to_file([r1, r2])
+        expr = [
+            {"y": 8, "x": 2, "id": r1.id, "width": 10, "height": 7},
+            {"y": 0, "x": 0, "id": r2.id, "width": 2, "height": 4}
+            ]
+        with open("Rectangle.json", "r") as file:
+            realr = eval(file.read())
+        self.assertEqual(expr, realr)
+
+        s1 = Square(10, 2, 8)
+        s2 = Square(2, 4)
+
+        Square.save_to_file([s1, s2])
+        exps = [
+            {"y": 8, "x": 2, "id": s1.id, "size": 10},
+            {"y": 0, "x": 4, "id": s2.id, "size": 2}
+            ]
+        with open("Square.json", "r") as file:
+            reals = eval(file.read())
+        self.assertEqual(exps, reals)
+
+        def test_overwrite(self):
+            r1 = Rectangle(10, 7, 2, 8)
+            Rectangle.save_to_file([r1])
+            exp1 = [{"y": 8, "x": 2, "id": r1.id, "width": 10, "height": 7}]
+            with open("Rectangle.json", "r") as file:
+                real1 = eval(file.read())
+            self.assertEqual(exp1, real1)
+
+            r2 = Rectangle(2, 4)
+            Rectangle.save_to_file([r2])
+            exp2 = [{"y": 0, "x": 0, "id": r2.id, "width": 2, "height": 4}]
+            with open("Rectangle.json", "r") as file:
+                real2 = eval(file.read())
+            self.assertEqual(exp2, real2)
