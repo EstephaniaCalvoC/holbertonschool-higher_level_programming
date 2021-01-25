35397d48a56f877cf03247a2a8eb0ae9833fa3e3
EstephaniaCalvoC <2177@holbertonschool.com>
2021-01-24 19:03:08 -0500

    Add to_dictionary method to class base task->15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!usr/bin/python3
 """Define a class Base"""
+import json
 
 
 class Base:
@@ -27,3 +28,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -5,6 +5,7 @@ Define unittests for Base class
 import os
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase_init(unittest.TestCase):
@@ -26,3 +27,29 @@ class TestBase_init(unittest.TestCase):
         b5 = Base()
         l_id = [b4.id, b9.id, b5.id]
         self.assertEqual([4, 9, 5], l_id)
+
+
+class TestBase_to_json_string(unittest.TestCase):
+    """Test cases for to_json_string method in Base class"""
+
+    def test_list_None(self):
+        self.assertEqual("[]", Base.to_json_string(None))
+
+    def test_list_empty(self):
+        self.assertEqual("[]", Base.to_json_string([]))
+
+    def test_correct_list(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dic1 = r1.to_dictionary()
+        r2 = Rectangle(20, 14, 4, 16)
+        dic2 = r2.to_dictionary()
+
+        json_dictionary = Base.to_json_string([dic1, dic2])
+        self.assertEqual(str, type(json_dictionary))
+
+        exp = [
+            {"x": 2, "width": 10, "id": r1.id, "height": 7, "y": 8},
+            {"x": 4, "width": 20, "id": r2.id, "height": 14, "y": 16}
+            ]
+        real = eval(json_dictionary)
+        self.assertEqual(exp, real) 
