3847a12181961b4b89754b23098b7181a75edbda
Estephania Calvo Carvajal <2177@holbertonschool.com>
2021-03-22 21:31:44 +0000

    Fix valitation

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const len = process.argv.length;
 
-if (len <= 2) {
+if (len <= 3) {
   console.log('0');
 } else {
   const nums = process.argv.slice(2, len).map(Number).sort().reverse();
