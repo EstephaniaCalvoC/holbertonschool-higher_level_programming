ebcb8eec8b4e7b60325ff1c8cd818c7d3f352822
Estephania Calvo Carvajal <2177@holbertonschool.com>
2021-03-23 01:53:12 +0000

    Fix sort function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,9 @@ const len = process.argv.length;
 if (len <= 3) {
   console.log('0');
 } else {
-  const nums = process.argv.slice(2, len).map(Number).sort().reverse();
+  const nums = process.argv.slice(2, len).map(Number).sort((a, b) => a - b).reverse();
+	// const nums = process.argv.slice(2, len).map(Number).sort((a, b) => b - a).reverse();
+	// const nums = process.argv.slice(2, len).map(Number).sort().reverse();
+	console.log(nums);
   console.log(nums[1]);
 }
