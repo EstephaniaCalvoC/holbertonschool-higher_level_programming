3fabee53f69d7a3f4e5735ac9155dc5afe8df52b
Estephania Calvo Carvajal <2177@holbertonschool.com>
2021-03-23 17:44:44 +0000

    Add class name

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle.js');
 
-module.exports = class extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
