a892e50d9ce9cb8b10e449c1661e4921d2e0b4bc
Estephania Calvo Carvajal <2177@holbertonschool.com>
2021-03-23 01:59:40 +0000

    Fix style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,6 @@ if (len <= 3) {
   console.log('0');
 } else {
   const nums = process.argv.slice(2, len).map(Number).sort((a, b) => a - b).reverse();
-	// const nums = process.argv.slice(2, len).map(Number).sort((a, b) => b - a).reverse();
-	// const nums = process.argv.slice(2, len).map(Number).sort().reverse();
-	console.log(nums);
+  console.log(nums);
   console.log(nums[1]);
 }
