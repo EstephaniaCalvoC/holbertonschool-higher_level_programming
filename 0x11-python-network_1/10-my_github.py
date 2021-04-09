95497f3cdc07d946a59987d7efed0b677da1a365
Estephania Calvo <2177@holbertonschool.com>
2021-04-09 19:48:33 +0000

    Add 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Display the user GitHub id"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    s_user = sys.argv[1]
+    s_token = sys.argv[2]
+
+    s_url = 'https://api.github.com/user'
+    r = requests.get(s_url, auth=(s_user, s_token))
+
+    r_id = r.json()['id']
+    print(r_id)
