7d5a18f3100eb7dfc7c316b4417fff48cba664da
Estephania Calvo <2177@holbertonschool.com>
2021-04-09 19:53:25 +0000

    Use get intead index

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     s_url = 'https://api.github.com/user'
     r = requests.get(s_url, auth=(s_user, s_token))
 
-    r_id = r.json()['id']
+    r_id = r.json().get('id')
     print(r_id)
